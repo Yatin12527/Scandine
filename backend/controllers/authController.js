72eae0559c810f798f8a7aaec90f30c4b621ca68
Yatinder <chhoker.yatinder123@gmail.com>
2025-09-06 19:10:53 +0530

    still checking

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,7 +40,7 @@ export const signup = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
   return res.status(200).json({
