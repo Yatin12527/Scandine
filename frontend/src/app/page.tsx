fe2d152d189536a388d8b80e9f2112bb65d42500
Yatinder <chhoker.yatinder123@gmail.com>
2025-10-28 23:27:06 +0530

    build fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { ArrowRight, Sparkles } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Footer from "@/components/footer";
